017c940fe9ae64563ffdc22320b97592c11de853
NazmulTakbir <nazmultakbir98@gmail.com>
2023-07-21 21:34:41 +0600

    adding tracking extensions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Helmet } from "react-helmet";
 
 import HomePage from "./Home/HomePage";
 
 function App() {
+  useEffect(() => {
+    if (window.location.pathname === "/i") {
+      console.log("Visiting From Instagram");
+    } else if (window.location.pathname === "/f") {
+      console.log("Visiting From Facebook");
+    } else if (window.location.pathname === "/l") {
+      console.log("Visiting From LinkedIn");
+    } else if (window.location.pathname === "/g") {
+      console.log("Visiting From Github");
+    } else if (window.location.pathname === "/k") {
+      console.log("Visiting From Kaggle");
+    } else if (window.location.pathname === "/t") {
+      console.log("Visiting From Twitter");
+    } else if (window.location.pathname === "/y") {
+      console.log("Visiting From Youtube");
+    }
+  }, []);
+
   return (
     <React.Fragment>
       <Helmet>
