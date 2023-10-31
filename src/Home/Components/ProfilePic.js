29db59de068fdaf25334c9134bed63a4b4a8078e
NazmulTakbir <nazmultakbir98@gmail.com>
2023-10-31 22:51:08 +0600

    Updated

--- src/Home/Components/ProfilePic.js
+++ src/Home/Components/ProfilePic.js
@@ -11,7 +11,7 @@ function ProfilePic() {
     <div class="row justify-content-center">
       <div class="col-lg-8 col-md-10">
         <img
-          src={require("../../Files/imgs/NazmulTakbir8.jpg")}
+          src={require("../../Files/imgs/NazmulTakbir9.jpeg")}
           class="img-fluid"
           alt="Nazmul Takbir"
           style={style}
